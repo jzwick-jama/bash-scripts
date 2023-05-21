b5c6b5a7438cc55486c504116fbe95beb696a620
Jenna Zwick <jennazwick888@gmail.com>
2023-05-21 13:33:29 -0700

    corrected issue with mysql setup script

--- jamanative-setup/configure-mysql-db.sql
+++ jamanative-setup/configure-mysql-db.sql
@@ -1,20 +1,27 @@
+CONFIGMYSQL //
+
 -- Check if MySQL version is 8 or higher
-SELECT VERSION() INTO @mysql_version;
-IF SUBSTRING_INDEX(@mysql_version, '.', 1) >= 8 THEN
-  -- Create databases
-  CREATE DATABASE IF NOT EXISTS jama CHARACTER SET utf8mb4;
-  CREATE DATABASE IF NOT EXISTS saml;
-  CREATE DATABASE IF NOT EXISTS oauth;
+CREATE PROCEDURE verify_version_and_setup()
+BEGIN
+  SELECT VERSION() INTO @mysql_version;
+  IF SUBSTRING_INDEX(@mysql_version, '.', 1) >= 8 THEN
+    -- Create databases
+    CREATE DATABASE IF NOT EXISTS jama CHARACTER SET utf8mb4;
+    CREATE DATABASE IF NOT EXISTS saml;
+    CREATE DATABASE IF NOT EXISTS oauth;
 
-  -- Create users
-  CREATE USER IF NOT EXISTS 'jamauser'@'%' IDENTIFIED BY 'password';
-  CREATE USER IF NOT EXISTS 'oauthuser'@'%' IDENTIFIED BY 'password';
-  CREATE USER IF NOT EXISTS 'samluser'@'%' IDENTIFIED BY 'password';
+    -- Create users
+    CREATE USER IF NOT EXISTS 'jamauser'@'%' IDENTIFIED BY 'password';
+    CREATE USER IF NOT EXISTS 'oauthuser'@'%' IDENTIFIED BY 'password';
+    CREATE USER IF NOT EXISTS 'samluser'@'%' IDENTIFIED BY 'password';
 
-  -- Grant privileges
-  GRANT ALL PRIVILEGES ON jama.* TO 'jamauser'@'%';
-  GRANT ALL PRIVILEGES ON oauth.* TO 'oauthuser'@'%';
-  GRANT ALL PRIVILEGES ON saml.* TO 'samluser'@'%';
-ELSE
-  SELECT 'MySQL version must be 8 or higher.';
-END IF;
+    -- Grant privileges
+    GRANT ALL PRIVILEGES ON jama.* TO 'jamauser'@'%';
+    GRANT ALL PRIVILEGES ON oauth.* TO 'oauthuser'@'%';
+    GRANT ALL PRIVILEGES ON saml.* TO 'samluser'@'%';
+  ELSE
+    SELECT 'MySQL version must be 8 or higher.';
+  END IF;
+END //
+
+CONFIGMYSQL ;
