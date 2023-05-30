ff954fad3475d6b5156b6f2afdd65eca8a3dadfa
Jenna Zwick <jennazwick888@gmail.com>
2023-05-30 16:30:00 +0000

    adds ssh-keygen to setup.sh and cats it to console & gives user SSH key page URL

--- ubuntu-setup/ubuntu-setup/setup.sh
+++ ubuntu-setup/ubuntu-setup/setup.sh
@@ -6,6 +6,10 @@ read -p "Please copy the following code and paste into a new 00-installer-config
 cat example.00-installer-config.yaml;
 read -p "Press Enter when ready to continue   ";
 sudo nano /etc/netplan/00-intaller-config.yaml;
-read -p "Set up github globals"
+read -p "Set up github globals and generating ssh keys";
+echo "Please go to https://github.com/settings/keys to add this new ssh key to Github";
+ssh-keygen;
+cat ~/.ssh/id_rsa.pub;
+read -p "Press enter to continue once you have copied your SSH key and added it to Github.";
 sudo apt-get install git -y; git config --global user.email "jennazwick888@gmail.com"; git config --global user.name "Jenna Zwick";
 echo "Setup complete."
